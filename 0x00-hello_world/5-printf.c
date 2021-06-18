d623b4266607d8be7da277d790f9379cb37febf5
Migdalis <miglepage@gmail.com>
2021-06-17 23:33:09 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - display a text
+ *Description: Function for display a text
+ *Return: 0 to success
+ *
+ *Display a text on screen
+ **/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
