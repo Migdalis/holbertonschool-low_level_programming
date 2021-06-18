074f0bb6147c68fcc05b5641dcbc65be73f64702
Migdalis <miglepage@gmail.com>
2021-06-18 10:04:29 -0700

    last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
   /* your code goes there */
 if (n > 0)
-printf("%ld is positive", n);
+printf("%d is positive\n", n);
 else
 {
 if (n < 0)
-printf("%ld is negative", n);
+printf("%d is negative\n", n);
 else
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#tdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
@@ -18,13 +18,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
   /* your code goes there */
 if (n > 5)
-printf("Last digit of %ld is %d and is greater than 5", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, (n%10));
 else
 {
 if (n == 0)
-printf("Last digit of %ld is %d and is 0", n);
+printf("Last digit of %d is %d and is 0\n", n, (n%10));
 else
-printf ("Last digit of %ld is %d and is less than 6 and not 0", n);
+if ((n < 6) && (n != 0))
+printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, (n%10));
 }
 return (0);
 }
