ba2ec8993141782dd12a4ef4c88155a1c02bf6c1
Migdalis <miglepage@gmail.com>
2021-06-17 23:26:57 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
-/* This function display a text */
+/**
+ *main - display a text
+ *Description: Function for display a text
+ *Return: 0 to success
+ *
+ *Display a text on screen
+ **/
 int main(void)
 {
+  /**
+   *main - display a text
+   *Description: Function for display a text
+   *Return: 0 to success
+   *
+   *Display a text on screen
+   **/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
