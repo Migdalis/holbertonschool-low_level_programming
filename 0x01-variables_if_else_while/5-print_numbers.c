e2474aa748c9c0af63c4eba6d354f596a8b83408
Migdalis <miglepage@gmail.com>
2021-06-18 20:04:43 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - display the numbers 1-10
+ *Description: Function for display numbers
+ *Return: 0 to success
+ *
+ *Display a sequence of numbers on screen
+ **/
+int main(void)
+{
+int i;
+for (i = 0; i < 10 ; i++)
+{
+printf("%d",i);
+}
+printf("\n");
+return (0);
+}
