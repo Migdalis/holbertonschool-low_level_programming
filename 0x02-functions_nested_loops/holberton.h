45478bb1c059acf9896040d49514d4f6f4288e9d
Migdalis <miglepage@gmail.com>
2021-06-22 16:46:13 -0700

    re-write

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,24 +1,3 @@
-#include <stdio.h>
-#include <string.h>
+#define HOLBERTON_H
 
-/**
- *Function to print a word on screen
- */
-int printofword (char word[])
-{
-  long unsigned int i;
-  for (i = 0; i < strlen(word); i++){
-    putchar(word[i]);
-  }
-  return (0);
-}
-
-void print_alphabet(void)
-{
-  int i;
-  for (i = 97; i < 123; i++)
-    {
-     putchar(i);
-    }
-  putchar(10);
-}
+int _putchar(char c);
