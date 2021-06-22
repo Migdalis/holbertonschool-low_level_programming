78a6e89196cdead45e16730209959c2a404e407f
Migdalis <miglepage@gmail.com>
2021-06-22 06:30:44 -0700

    say holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Prints Holberton
+ *Description: Function for prints a word
+ *Return: 0 to success
+ *
+ *Display a text on screen
+ **/
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
