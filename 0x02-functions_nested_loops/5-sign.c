fd8c23e7639499af4158fcbfb4fd67ba905ae1f8
Migdalis <miglepage@gmail.com>
2021-06-22 18:13:30 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *print_sign - Prints the sign of a number
+ *@c: The number to check
+ *Description: Function for prints a sign
+ *Return: 1 to greater than zero, 0 to zero or -1 to less than zero
+ *
+ **/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
