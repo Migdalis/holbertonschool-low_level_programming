33cf12e423511b878ed8e0cb293b7f88618f0be1
Migdalis <miglepage@gmail.com>
2021-06-24 16:22:05 -0700

    Init debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio>
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 98;
+printf("Function succes: %d", positive_or_negative(i);
+
+return (0);
+}
