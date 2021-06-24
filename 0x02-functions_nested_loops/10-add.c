ef9522439a3d4a8cc735bae5d2633f361eddabf4
Migdalis <miglepage@gmail.com>
2021-06-23 20:28:41 -0700

    suma

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *add - Prints the sum of two numbers
+ *@a: Number to sum
+ *@b: Number to sum
+ *Description: Function for sum two numbers
+ *Return: The result of sum
+ *
+ **/
+
+int add(int a, int b)
+{
+return(a + b);
+}
