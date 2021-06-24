189c4d727c23743147a168d249cca76e83b52245
Migdalis <miglepage@gmail.com>
2021-06-23 21:54:16 -0700

    natural sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,24 +3,25 @@
 
 /**
  *main - Prints the sum of all numbers multiples of 3 or 5
- *Description: Function for prints sum all multiples of 3 or 5 between 0 to 1024
+ *Description: Function for prints sum all multiples
+ *of 3 or 5 between 0 to 1024
  *Return: 0
  *
  **/
 
 int main(void)
 {
-  int sum;
-  int i;
+int sum;
+int i;
 
-  sum = 0;
-  for (i = 0; i < 1024; i++)
-    {
-      if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	  sum += i;
-	}
-    }
-  printf("%d", sum);
-  return (0);
+sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
 }
