2138f2824bbdc65e62289cdf97fc21aa9cc46579
Migdalis <miglepage@gmail.com>
2021-06-25 09:22:00 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *print_diagonal - Prints a line diagonal
+ *@n: times to print
+ *Description: Function for prints a line n times
+ *
+ **/
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j <= i; j++)
+{
+if (j == i)
+{
+_putchar(92);
+_putchar(10);
+}
+_putchar(32);
+}
+}
+}
+else
+_putchar(10);
+}
