77e6fd225e0d44f59ed2e6c631d2cf13bae2426f
Migdalis <miglepage@gmail.com>
2021-06-24 21:09:14 -0700

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *mul - Mult two integers
+ *@a: Integer to mult
+ *@b: Integer to mult
+ *Description: Function for mult two int
+ *Return: the result of mult
+ *
+ **/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
