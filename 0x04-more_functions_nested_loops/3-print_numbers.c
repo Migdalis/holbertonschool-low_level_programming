05f43917efb92638dda51401429476836ab0ce05
Migdalis <miglepage@gmail.com>
2021-06-24 21:16:27 -0700

    print num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - Prints numbers 0 to 9
+ *Description: Function for prints numbers
+ *
+ **/
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+
+_putchar(10);
+}
