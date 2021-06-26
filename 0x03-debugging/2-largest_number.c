9f33fb09bedce5abd16858bf00711e00b2f97236
Migdalis <miglepage@gmail.com>
2021-06-25 21:53:55 -0700

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,10 +14,15 @@ int largest;
 
 if ((a > b) && (a > c))
 largest = a;
+else
+{
 if ((b > a) && (b > c))
 largest = b;
+else
+{
 if ((c > a) && (c > b))
 largest = c;
-
+}
+}
 return (largest);
 }
