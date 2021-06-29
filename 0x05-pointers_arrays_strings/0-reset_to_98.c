10aab35a50ec35c86c5e7ca996e6356f6c2ce31a
Migdalis <miglepage@gmail.com>
2021-06-29 05:09:06 -0700

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - Change a value
+ *@n: Pointer to change
+ *Description: Function for change the value of pointer
+ *
+ **/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
