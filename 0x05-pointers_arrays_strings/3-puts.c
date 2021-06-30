2671ae4d050fd922ba44a015ef4a79f6bf12f48b
Migdalis <miglepage@gmail.com>
2021-06-29 21:20:37 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_puts - Prints a string
+ *@*s: Pointer to string
+ *Description: Function for prints a string
+ *Return: void
+ *
+ **/
+
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+while (*(str+i) != '\0')
+{
+_putchar(*(str+i));
+i++;
+}
+
+_putchar(10);
+}
