51bcd8e13c504efc23f52d4b226b50b0a1c83c22
Migdalis <miglepage@gmail.com>
2021-06-30 20:56:35 -0700

    array int

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_array - Prints an array
+ *@a: Pointer to array
+ *@n: Elements to prints
+ *Description: Function for prints n elements of an array
+ *Return: void
+ *
+ **/
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i == (n - 1))
+{
+printf("%d\n", a[i]);
+break;
+}
+printf("%d, ", a[i]);
+}
+}
