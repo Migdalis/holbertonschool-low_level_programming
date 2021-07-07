921872e4b45b36dcd00601ed8434920fce4c10d7
Migdalis <miglepage@gmail.com>
2021-07-06 21:33:47 -0700

    cpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,27 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+  int i;
 
-i = 0;
-while ((*(src + i) != '\0') && (i < n))
-{
-dest[i] = src[i];
-i++;
-}
-if (src[i] == '\0')
-dest[i] = '\0';
-return (dest);
+  i = 0;
+  while ((*(src + i) != '\0'))
+    {
+      i++;
+    }
+
+  if (i > 0)
+    {
+      int j;
+      j = 0;
+      while ((*(src + j) != '\0') && (j < n))
+	{
+	  dest[j] = src[j];
+	  j++;
+	}
+
+    }
+  if (src[i] == '\0')
+    dest[i] = '\0';
+
+  return (dest);
 }
