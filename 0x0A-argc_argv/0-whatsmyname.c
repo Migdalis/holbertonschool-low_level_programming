b97f6b9c759bf82b51e5cc23e19762b5669775e7
Migdalis <miglepage@gmail.com>
2021-07-08 11:41:32 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *main - Print my name
+ *@argc: Size of argv
+ *@argv: Argument vector
+ *Description: Print name of program using argv
+ *Return: 0
+ *
+ **/
+
+int main(int argc, char *argv[])
+{
+  if (argc)
+  printf("%s\n", argv[0]);
+
+  return (0);
+}
