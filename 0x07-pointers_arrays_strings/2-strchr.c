089585d9fd97512c28ecbda76054c633c43da763
Migdalis <miglepage@gmail.com>
2021-07-07 20:54:42 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ *_strchr - Locates a character
+ *@s: String
+ *@c: Characer to locate
+ *Description: Function that locates a character in a string
+ *Return: A pointer to the ocurrence of the character
+ *
+ **/
+
+char *_strchr(char *s, char c)
+{
+int i;
+
+i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s+i);
+}
+
+i++;
+}
+
+return (NULL);
+}
