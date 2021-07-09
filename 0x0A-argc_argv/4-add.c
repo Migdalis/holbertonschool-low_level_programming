4b301545125cfa435f0006bc8841d9bb9aef1545
Migdalis <miglepage@gmail.com>
2021-07-08 17:44:37 -0700

    Adds num

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,21 +12,21 @@
 
 int main(int argc, char *argv[])
 {
-int i;
+int i, j;
 int sum = 0;
 if (argc > 0)
 {
 for (i = 1; i < argc; i++)
 {
-if (atoi(argv[i]))
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-sum += atoi(argv[i]);
-}
-else
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("Error\n");
 return (1);
 }
+}
+sum += atoi(argv[i]);
 }
 }
 
