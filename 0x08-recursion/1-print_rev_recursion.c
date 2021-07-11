943c2c8f8c2beb7275e667c9b05cb5f2f87c2a11
Migdalis <miglepage@gmail.com>
2021-07-10 21:36:21 -0700

    Puts in reverse with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - Print a string in reverse
+ *@s: String to print
+ *Description: Function that print a string in reverse with recursion
+ *
+ **/
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
