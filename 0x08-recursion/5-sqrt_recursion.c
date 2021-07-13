d21442616929465702e4cb0d4b9e55142f2ef815
Migdalis <miglepage@gmail.com>
2021-07-12 19:33:09 -0700

    Raiz cuadrada natural de un numero

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ *_sqrt_recursion - Square root of a number
+ *@n: Number to find square root
+ *Description: Function that find the natural square root of a number
+ *Return: A integer to square root, -1 if not have a natural square
+ *
+ **/
+
+int _sqrt_recursion(int n)
+{
+int temp, num_sup, num_inf, i;
+
+num_inf = 0;
+num_sup = n;
+i = 0;
+if ((n > 0) & (n != 1))
+{
+while (i < num_sup)
+{
+temp = (num_sup + num_inf) / 2;
+if ((temp *temp) == n)
+{
+return (temp);
+}
+else
+{
+if ((temp *temp) > n)
+{
+num_sup = temp;
+}
+else
+{
+num_inf = temp;
+}
+}
+i++;
+}
+return (-1);
+}
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return ((n == 0) ? 0 : 1);
+}
+}
