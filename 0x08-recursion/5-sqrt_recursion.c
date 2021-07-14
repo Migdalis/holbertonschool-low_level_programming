465fef05aa19d0650e9f67b268051b7a46047d7e
Migdalis <miglepage@gmail.com>
2021-07-13 21:55:52 -0700

    Square root recursive :D

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,41 @@
 #include "holberton.h"
 
 /**
+ *search_root - Square root of a number
+ *@raiz: Number to find square root
+ *@limit_sup: Acortar busqueda en el rango
+ *@limit_inf: Acortar busqueda en el rango
+ *Description: Function that find the natural square root of a number
+ *Return: A integer to square root, -1 if not have a natural square
+ *
+ **/
+int search_root(int raiz, int limit_sup, int limit_inf)
+{
+int temp;
+
+temp = (limit_sup + limit_inf) / 2;
+
+if ((temp *temp) == raiz)
+{
+return (temp);
+}
+else
+{
+if ((temp *temp) > raiz)
+{
+return (search_root(raiz, temp, limit_inf));
+}
+else
+{
+if (temp == limit_inf)
+{
+return (-1);
+}
+return (search_root(raiz, limit_sup, temp));
+}
+}
+}
+/**
  *_sqrt_recursion - Square root of a number
  *@n: Number to find square root
  *Description: Function that find the natural square root of a number
@@ -9,28 +44,16 @@
  **/
 int _sqrt_recursion(int n)
 {
-int temp, num_sup, num_inf, i;
-
-num_inf = 0;
-num_sup = n;
-i = 0;
 if ((n > 0) & (n != 1))
 {
-while (i < num_sup)
-{
-temp = (num_sup + num_inf) / 2;
-if ((temp *temp) == n)
-return (temp);
-if ((temp *temp) > n)
-num_sup = temp;
-else
-num_inf = temp;
-i++;
-}
-return (-1);
+return (search_root(n, n, 0));
 }
 if (n < 0)
+{
 return (-1);
+}
 else
+{
 return ((n == 0) ? 0 : 1);
+}
 }
