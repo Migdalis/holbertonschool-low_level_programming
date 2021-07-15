aa5e9f3c9ae46d0445fac285c60175d4ac00318b
Migdalis <miglepage@gmail.com>
2021-07-15 09:13:23 -0700

    Fix alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,20 +19,25 @@ int i, j;
 if ((width <= 0) || (height <= 0))
 return (NULL);
 
-array2D = malloc(height * sizeof(int));
-
+array2D = malloc(height * sizeof(int *));
 if (!array2D)
 return (NULL);
 for (i = 0; i < height; i++)
 {
 array2D[i] = malloc(width * sizeof(int));
 if (!array2D[i])
+{
+for (j = 0; j < i; j++)
+{
+free(array2D[j]);
+}
+free(array2D);
 return (NULL);
+}
 for (j = 0; j < width; j++)
 {
 array2D[i][j] = 0;
 }
 }
 return (array2D);
-
 }
