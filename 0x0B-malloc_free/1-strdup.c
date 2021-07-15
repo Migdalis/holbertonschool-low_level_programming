106b1a74236e73de88aaf46851b645b0f1845fe0
Migdalis <miglepage@gmail.com>
2021-07-14 20:51:22 -0700

    Copy a string ina new pointer with mallos

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "holberton.h"
+
+/**
+ *_strdup - Copy of a string
+ *@str: String to copy
+ *Description: Function that copy a string given as a parameter
+ *Return: A pointer to new string or null if it fails
+ *
+ **/
+
+char *_strdup(char *str)
+{
+char *new_array;
+int i, size;
+
+if (str == NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0'; i++)
+{
+}
+
+size = i + 1;
+new_array = malloc(i * sizeof(char));
+
+if (new_array)
+{
+for (i = 0; i < size; i++)
+{
+new_array[i] = str[i];
+}
+
+return (new_array);
+}
+
+return (NULL);
+}
