56617ae871995c6a4677864a0c184fd28c030cb4
Migdalis <miglepage@gmail.com>
2021-07-15 21:30:50 -0700

    Free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "holberton.h"
+
+/**
+ *free_grid - Release memory space of an array 2D of integers
+ *@grid: Pointer to array release
+ *@height: Height of array to release
+ *Description: Function that frees a 2 dimensional grid previously created
+ *
+ **/
+
+void free_grid(int **grid, int height)
+{
+  int i;
+  if (grid != NULL && height > 0)
+    {
+      for (i = 0; i < height; i++)
+	{
+	  free(grid[i]);
+	}
+      free(grid);
+    }
+}
