ea02ba4f52f67b089d4bff29f1900accecb76f28
Migdalis <miglepage@gmail.com>
2021-07-17 04:22:01 -0700

    Allocates memory with malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ *malloc_checked - Allocates memory
+ *@b: Size a new memory
+ *Description: Function that allocates memory using malloc
+ *Return: A pointer or 98 it fails
+ *
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+void *new_memory;
+
+new_memory = malloc(b * sizeof(void *));
+
+if (new_memory)
+return (new_memory);
+else
+exit(98);
+}
