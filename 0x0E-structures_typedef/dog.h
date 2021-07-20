dfcc7bd1b66ae9f00fe9bcfc0d6844b1dfc084cd
Migdalis <miglepage@gmail.com>
2021-07-19 21:34:14 -0700

    dog library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Data on a dog
+ * @name: Name string
+ * @age: Age float
+ * @owner: Name of owner string
+ *
+ * Description: Data about a dog
+ */
 typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 } dog;
 
 #endif /*DOG_H*/
