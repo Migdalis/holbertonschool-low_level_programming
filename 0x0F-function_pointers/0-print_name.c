4af7acda30fd3d97870f474c79a0cf4245b5e6a5
Migdalis <miglepage@gmail.com>
2021-07-21 18:31:30 -0700

    First pointer function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - Prints a name
+ *@name: Name to print
+ *@f: Pointer to Function
+ *Description: Function that prints a name
+ *
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+void (*fun_point)(char *) = f;
+
+fun_point(name);
+}
