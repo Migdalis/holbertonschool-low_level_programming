911cdeb307ad85b20d6eeb3c1c4b563c4b3336c8
Migdalis <miglepage@gmail.com>
2021-07-31 21:58:49 -0700

    Len list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - Function that returns the number of elements in a linked list
+ * @h: Head of a list
+ * Description: Function that returns the number of elements in a linked list
+ * Return: The number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
