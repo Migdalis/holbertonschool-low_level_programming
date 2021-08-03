911ccda38a370f47d9db7018f2cf6f329f2d4f52
Migdalis <miglepage@gmail.com>
2021-08-03 16:41:25 -0500

    Free 2.0

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	while (*head)
+	if (*head)
 	{
-		aux = *head;
-		*head = (*head)->next;
-		free(aux);
+		while (*head)
+		{
+			aux = *head;
+			*head = (*head)->next;
+			free(aux);
+		}
 	}
 }
