e0b85fc177e24f951134614e22ac1727a80b4ef9
Migdalis <miglepage@gmail.com>
2021-08-03 20:42:34 -0500

    Insert node in n position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	if (idx == 0 || !aux)
+	if (idx == 0 || *head == NULL)
 	{
 		new->next = *head;
 		*head = new;
