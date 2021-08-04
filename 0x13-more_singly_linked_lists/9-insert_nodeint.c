73634524c72275773207cebcc7bacfdb7234de08
Migdalis <miglepage@gmail.com>
2021-08-03 19:38:59 -0500

    Insert node in n position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	aux = *head;
+	if (idx > 0 && aux == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
@@ -35,5 +37,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		aux = aux->next;
 	}
 
-	return (NULL);	
+	return (NULL);
 }
