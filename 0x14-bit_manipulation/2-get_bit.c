9769bb566d9e80f28cd36cc44f1d51af7d44e800
Migdalis <miglepage@gmail.com>
2021-08-05 22:30:04 -0500

    Bit given

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,8 +14,6 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	if (n == 0 && index > 0)
-		return (-1);
 	if (n == 0 && index == 0)
 		return (0);
 	while (copy)
@@ -23,7 +21,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		copy = copy >> 1;
 		i++;
 	}
-	if (index > i - 1 || index >= (sizeof(unsigned long int) * 8))
+	if (index > (i - 1))
 		return (-1);
 	return ((n >> index) & 1);
 }
