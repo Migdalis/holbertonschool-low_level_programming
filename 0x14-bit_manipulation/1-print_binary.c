0ad6bd3372ac8fa8eee93a8dcf7e87a90b4708f0
Migdalis <miglepage@gmail.com>
2021-08-05 21:24:14 -0500

    Pleeeease

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,17 +2,6 @@
 #include "main.h"
 
 /**
- * pow_btwo - Function that calculate a pow with base 2
- * @power: Elevate
- * Return: The result of power
- */
-unsigned long int pow_btwo(unsigned long int power)
-{
-	if (power == 0)
-		return (1);
-	return (pow_btwo(power - 1) * 2);
-}
-/**
  * print_binary - Function that prints the binary representation of a number
  * @n: Decimal number
  */
@@ -30,12 +19,10 @@ void print_binary(unsigned long int n)
 			i++;
 		}
 		po = i - 1;
-		i = po;
-		while (n >> i)
+		while (n >> po)
 		{
-			_putchar('0' + ((n >> i) & 1));
-			i--;
+			_putchar('0' + ((n >> po) & 1));
+			po--;
 		}
 	}
-	
 }
