0edd5e8c6dd9fca97ec9ee4ee2156e7bf01d0f7d
Migdalis <miglepage@gmail.com>
2021-09-30 22:52:34 -0500

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - display a text
+ *Description: Function for display a text
+ *Return: 1 to success
+ **/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
