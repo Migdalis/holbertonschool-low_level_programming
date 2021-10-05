bc67a46d604f9eb3fb1c9eb5d544c61099a3ce8d
Migdalis <miglepage@gmail.com>
2021-10-05 11:45:31 -0500

    Change files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ *main - Prints _putchar
+ *Description: Function for prints a word
+ *Return: 0 to success
+ *
+ *Display a text on screen
+ **/
+int main(void)
+{
+	unsigned long int i;
+	char word[] = "_putchar\n";
+	i = 0;
+	while (i < strlen(word))
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	return (0);
+}
