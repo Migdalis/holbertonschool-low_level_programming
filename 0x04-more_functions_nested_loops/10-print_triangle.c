725f428d6db5a71d9fdac5e66188f8e13f68d97f
Migdalis <miglepage@gmail.com>
2021-10-10 22:51:16 -0500

    Change files

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *print_triangle - Prints a triangle
@@ -9,27 +9,25 @@
 
 void print_triangle(int size)
 {
-int i;
-int j;
-int k;
+	int i;
+	int j;
+	int k;
 
-if (size > 0)
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 0; j < size - i; j++)
-{
-_putchar(32);
-}
-for (k = j; k < size; k++)
-{
-_putchar(35);
-}
-_putchar(10);
-}
-}
-else
-{
-_putchar(10);
-}
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(32);
+			}
+			for (k = j; k < size; k++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+	else
+		_putchar(10);
 }
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *print_number - Prints a integer
@@ -9,34 +9,32 @@
 
 void print_number(int n)
 {
-int abs;
+	int abs;
 
-if (n > 0)
-{
-if ((n / 10) != 0)
-{
-print_number(n / 10);
-_putchar('0' + n % 10);
-}
-else
-{
-_putchar('0' + n % 10);
-}
-return;
-}
+	if (n > 0)
+	{
+		if ((n / 10) != 0)
+		{
+			print_number(n / 10);
+			_putchar('0' + n % 10);
+		}
+		else
+			_putchar('0' + n % 10);
+		return;
+	}
 
-if (n < 0)
-{
-abs = (-1) * n;
+	if (n < 0)
+	{
+		abs = (-1) * n;
 
-if ((abs / 10) != 0)
-{
-_putchar(45);
-print_number(abs / 10);
-_putchar('0' + abs % 10);
-}
-}
+		if ((abs / 10) != 0)
+		{
+			_putchar(45);
+			print_number(abs / 10);
+			_putchar('0' + abs % 10);
+		}
+	}
 
-if (n == 0)
-_putchar(48);
+	if (n == 0)
+		_putchar(48);
 }
