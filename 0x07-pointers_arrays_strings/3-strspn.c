20914753961215d8dda16f6c1e50584ccef53e04
Migdalis <miglepage@gmail.com>
2021-10-18 16:22:07 -0500

    Add strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,8 +16,6 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int bytes = 1;
 
-	if (accept[0] == '\0')
-		return (0);
 	for (i = 0; accept[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
