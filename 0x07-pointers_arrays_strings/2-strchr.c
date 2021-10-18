9956dd63f231d66d8b864aa89c16ef8706098b98
Migdalis <miglepage@gmail.com>
2021-10-18 15:39:51 -0500

    Fix Find char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,15 +15,14 @@ char *_strchr(char *s, char c)
 	int i;
 
 	i = 0;
-	if (*s == c)
-		return (s);
-
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
+	if (s[i] == c)
+		return (s + i);
 
 	return (NULL);
 }
