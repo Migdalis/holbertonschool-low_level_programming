1d9a7fe6d9f99017fdab8ecebb96052599dda57a
Migdalis <miglepage@gmail.com>
2021-10-19 12:32:41 -0500

    Fix task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,18 +16,16 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int bytes = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0' && s[i] != ','; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
 				bytes += 1;
 				break;
 			}
 		}
 	}
-	if (bytes > 0)
-		bytes++;
 	return (bytes);
 }
