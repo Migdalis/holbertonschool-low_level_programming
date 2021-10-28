a5477b553c1ccfd27d43266b2848b128609e7fdf
Migdalis <miglepage@gmail.com>
2021-10-27 23:03:40 -0500

    Add first task advn

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ *argstostr - Concatenates all the arguments
+ *@ac: Size of argv
+ *@av: Argument vector
+ *Description: Function that concatenates all the arguments of your program
+ *Return: A pointer to a new string, or NULL if it fails
+ *
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, size = 0, k = 0;
+	char *new;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j] != '\0'; j++)
+			{
+				size++;
+			}
+		}
+		new = malloc((size + 1) * sizeof(char));
+		if (new)
+		{
+			for (i = 0; i < ac; i++)
+			{
+				for (j = 0; av[i][j] != '\0'; j++)
+				{
+					new[k] = av[i][j];
+					k++;
+				}
+				new[k++] = '\n';
+			}
+			return (new);
+		}
+	}
+	return (NULL);
+}
