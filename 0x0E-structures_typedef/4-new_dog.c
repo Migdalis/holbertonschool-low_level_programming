7bbae05bedb70141af6b6b05e111316e1d2ab552
Migdalis <miglepage@gmail.com>
2021-11-01 18:54:27 -0500

    Add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,6 +51,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new;
 	char *nam, *own;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	nam = _strdup(name);
 	own = _strdup(owner);
 	if (nam && own)
