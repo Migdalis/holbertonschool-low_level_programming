f9cc2a24a5368affbd2f99c471e1abaf16e44fa9
Migdalis <miglepage@gmail.com>
2021-11-01 15:02:31 -0500

    Add index

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,13 @@
 
 void print_dog(struct dog *d)
 {
-if (d != NULL)
-{
-printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-if (d->age < 0)
-printf("Age: (nil)\n");
-else
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
-}
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		if (d->age < 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
 }
