b1c0c1494711911bab4e41d708453bac7a0e78ef
Migdalis <miglepage@gmail.com>
2021-11-01 19:52:07 -0500

    Fix task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -38,27 +38,6 @@ char *_strdup(char *str)
 }
 
 /**
- *_strlen - Length of a string
- *@s: Pointer to string
- *Description: Function for get length of a string
- *Return: Integer of length
- *
- **/
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
  *new_dog - Create a new dog
  *@name: Name to set
  *@age: Age to set
