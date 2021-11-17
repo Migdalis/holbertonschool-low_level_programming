66b7e96beefd8f61260d869a831cc7258a11ba9c
Migdalis <miglepage@gmail.com>
2021-11-16 21:16:08 -0500

    Betty happy

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t count;
-count = 0;
+	size_t count = 0;
 
-while (h)
-{
-printf("%d\n", h->n);
-count++;
-h = h->next;
-}
-return (count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
