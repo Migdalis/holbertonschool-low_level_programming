e34cebdd64fba72d2e7775ff014de40f361c28aa
Migdalis <miglepage@gmail.com>
2021-11-16 22:52:13 -0500

    Task reverse list :D

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *next = NULL, *aux;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 	if ((*head)->next == NULL)
 		return (*head);
