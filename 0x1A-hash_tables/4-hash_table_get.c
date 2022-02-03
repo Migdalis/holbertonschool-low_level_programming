ccb8dad3fbb991d7eadef2241ee329181a757503
Migdalis <miglepage@gmail.com>
2022-02-02 20:47:32 -0500

    Fix while task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *aux;
 	unsigned long int idx;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL)
 		return (NULL);
 
 	idx = key_index((const unsigned char *)key, ht->size);
@@ -24,6 +24,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(aux->key, key) == 0)
 			return (aux->value);
+		aux = aux->next;
 	}
 	return (NULL);
 }
