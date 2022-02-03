796d7477e3a34de7d62529eb67c3f9aabddde256
Migdalis <miglepage@gmail.com>
2022-02-02 22:35:39 -0500

    Add task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table
+ * @ht: Pointer to the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp_one, *tmp_two;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp_one = ht->array[i];
+			while (tmp_one)
+			{
+				tmp_two = tmp_one->next;
+				free(tmp_one->key);
+				free(tmp_one->value);
+				free(tmp_one);
+				tmp_one = tmp_two;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
