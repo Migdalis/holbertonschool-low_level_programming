2d72eb3844309dc1b3964d9f9686f4456d18e27a
Migdalis <miglepage@gmail.com>
2022-02-03 12:10:26 -0500

    Fix task 6, I hope

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,7 +9,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp;
 	unsigned long int i;
 
-	if (ht == NULL || ht->array == NULL || ht->size < 0)
+	if (ht == NULL || ht->array == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
@@ -27,5 +27,6 @@ void hash_table_delete(hash_table_t *ht)
 		}
 	}
 	free(ht->array);
+	ht->array = NULL;
 	free(ht);
 }
